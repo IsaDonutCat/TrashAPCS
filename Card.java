55b66a26a15e9b98b8a06635ae018394b7178137
Isabella Wang <wangisa26@7hills.org>
2023-11-08 09:28:36 -0500

    created deck and cards, + vars and methods that would be expected of deck (draw and deal)

---
+++ Card.java
@@ -0,0 +1,37 @@
+public class Card
+{
+    boolean faceUp;
+    int numValue; //numerical value of card (0-12 in computer bc)
+    String name;
+
+    public Card (int InnumValue, String invalue, String insuit) //constructor class
+    {
+        name = invalue + " of " + insuit;
+        faceUp = false; //assume facedown position
+    }
+
+    public int getNumValue () //getter classes
+    {
+        return numValue;
+    }
+
+    public String getName ()
+    {
+        return name;
+    }
+
+    public boolean getFaceUp ()
+    {
+        return faceUp;
+    }
+
+    public void flipCard()
+    {
+        if (faceUp)
+            faceUp = false;
+        else
+            faceUp = true;
+
+        return;
+    }
+}
---
+++ Deck.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+
+public class Deck
+{
+    String[] suits = {"Hearts", "Diamonds", "Spades", "Clubs"};
+    String[] values = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+    Card temp;
+    int index;
+    ArrayList<Card> deck; //need to declare outsid for it to be used throughout the class
+
+    public Deck () //constructor
+    {
+        deck = new ArrayList<Card>();
+
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 13; j++) //cycle thru the values and j can be used as numValue
+                deck.add(new Card(j, values[j], suits[i]));
+        }
+    }
+
+    public Card deal() //deal random card (remain faceDown)
+    {
+        index = (int) (Math.random() * deck.size());
+        temp = deck.get(index);
+        deck.remove(index);
+        return temp;
+
+    }
+
+    public Card draw() // draw random card (Flip and print out value so player knows what it is)
+    {
+        index = (int) (Math.random() * deck.size());
+        deck.get(index).flipCard();
+        temp = deck.get(index);
+        deck.remove(index);
+        return temp;
+    }
+}
