8af4536c7f24bcae8b9590723be0db65f0376d61
IsaDonutCat <114428942+IsaDonutCat@users.noreply.github.com>
2023-11-08 15:56:13 -0500

    Update Grid.java

--- Grid.java
+++ Grid.java
@@ -1,44 +1,196 @@
+import Java.util.Scanner;
+
 public class Grid
 {
-    Card[][] cardGrid = new Card[2][5]// 2 rows 5 columns
-
+    Card[][] cardGrid; // 2 rows 5 columns
+    public boolean[][] blankSlot;
+    int numCards, tempRow, tempCol;
     ///default constructor class that takes nothing and does nothing already exists and im lazy
-
-    public void printGrid()
-    {
-        for (int i = 0; i < 2; i++)
+    public Grid(int size, Deck dealFrom) //constructor class, possible future recursion
+    {   
+        if (size <= 5) //create according to size, if five or less its only one row
+        {   
+            cardGrid = new Card[][] {new Card[size]};
+            blankSlot = new boolean[][] {new boolean[size]};
+        }
+        else
         {
-            for (int j = 0; j < 5; j++)
+            cardGrid = new Card[][] {new Card[5], new Card[size-5]}; //since first row def filled, its at lest 5|1
+            blankSlot = new boolean[][] {new boolean[5], new boolean[size-5]};
+        }
+        
+        for (int a = 0; a < cardGrid.length; a++) //array.length returns num rows
+        {       
+            for (int b = 0; b < cardGrid[a].length; b++)  //gets different length per
             {
-                if (!cardGrid[i][j].getFaceUp())
-                    System.out.print("?");
+                cardGrid[a][b] = dealFrom.deal();
+                blankSlot[a][b] = false; //initialize
+            }
+            System.out.println();
+        }
+
+        numCards = size;
+    }
+
+    public void printGrid() //print out what cards are known and what cards are unknown
+    {
+        for (int c = 0; c < cardGrid.length; c++) //array.length returns num rows
+        {       
+            for (int d = 0; d < cardGrid[c].length; d++) 
+            {
+                if (blankSlot[c][d])
+                    System.out.print(" ");
+                else if (cardGrid[c][d].getFaceUp())
+                    System.out.print(cardGrid[c][d].getName());
                 else
-                    System.out.print(cardGrid[i][j].getName());
-                if (j < 4) //so only prints in between
-                    System.out.print(" | ");
+                    System.out.print("?");
+
+                if (d < cardGrid[c].length - 1)
+                    System.out.print("|");
             }
             System.out.println();
         }
     }
 
-    public void printLayout()
+    public void printLayout() //print layout so user knows wh  
     {
-        for (int i = 0; i < 2; i++)
-        {
-            for (int j = 0; j < 5; j++)
+        int cursor = 0;//for continuous counting
+
+        for (int e = 0; e < cardGrid.length; e++) //array.length returns num rows
+        {       
+            cursor++;
+            
+            for (int f = 0; f < cardGrid[e].length; f++) 
             {
-                if (!cardGrid[i][j].getFaceUp())
-                    System.out.print(i*j + 1);
-                if (j < 4) //so only prints in between
-                    System.out.print(" | ");
+                System.out.print(cursor);
+
+                if (f < cardGrid[e].length - 1)
+                    System.out.print("|");
             }
             System.out.println();
         }
     }
 
-    public Card selectCard (int index)
+    public boolean anyUndrawn () //checks to see if this is even a valid option to draw from player grid
     {
-        printLayout();
+        for (int h = 0; h < cardGrid.length; h++) //array.length returns num rows
+        {       
+            for (int i = 0; i < cardGrid[h].length; i++) 
+            {
+                if(!cardGrid[h][i].getFaceUp())
+                    return true;
+            }
+        }
+        return false;
+    }
 
+    public boolean selectCard () //select facedown card in grid 
+    {
+        printGrid();
+        System.out.println();
+        printLayout();
+        Scanner input = new Scanner(System.in);
+        String answer;
+
+        do
+        {   
+            System.out.println("Enter the corresponding number for the card you want to flip over");
+            answer = input.nextLine();
+        }
+        while (possError(answer)); //run until valid choice
+
+        return drawCard(Integer.parseInt(answer)-1);
+    }
+
+    public boolean possError (String inChoice)//find any errors with answer
+    {
+        char[] arrDigits = inChoice.toCharArray();
+        int lenChoice = arrDigits.length;
+
+        for (int g = 0; g < lenChoice; g++)
+        {
+            if (!Character.isDigit(arrDigits[g]))
+            {
+                System.out.println("Please enter a number");
+                return true;
+            }
+        }
+
+        if (lenChoice > 2)
+        {
+            System.out.println("Please choose a number within the range of your cards");
+            return true;
+        }
+
+        int ans = Integer.parseInt(inChoice) -1; //okay to parseInt since completely integer
+
+        if (ans >= numCards) //error for not a possible slot
+        {
+            System.out.println("Please choose a number within the range of your cards");
+            return true;
+        }
+
+        if (cardGrid[ans/5][ans%5].getFaceUp()) //error for already existing card
+        {
+            System.out.println("You already have a face-up card here!");
+            return true;
+        }
+
+        if(blankSlot[ans/5][ans%5]) //error for no card to draw
+        {
+            System.out.println("There's no card here!");
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean drawCard (int index) //draw facedown card from grid
+    {
+        tempRow = index/5;
+        tempCol = index%5;
+        cardGrid[tempRow][tempCol].flipCard();
+        blankSlot[tempRow][tempCol] = true; //now that the card has been "drawn" the slot is blank
+        System.out.println("The card is " + cardGrid[tempRow][tempCol].getName());//reveal value
+        return placeCard(cardGrid[tempRow][tempCol]); //place card will be chosen
+    }
+
+    public boolean placeCard (Card inHand)//place card in grid, return false to end turn
+    {   
+        int cardVal = inHand.getNumValue();
+        if (cardVal >= numCards)
+        {
+            System.out.println("Trash!");
+            return false; //the card is trash
+        }
+        else if (blankSlot[tempRow][tempCol]) //do this first since im too lazy to remove a drawn card from the cardGrid
+        {
+            cardGrid[tempRow][tempCol] = inHand;
+            blankSlot[tempRow][tempCol] = false;
+            return false; //
+        }
+        else if (cardGrid[tempRow][tempCol].getFaceUp())//the player already has this value 
+        {
+            System.out.println("Trash!");
+            return false; //the card is trash
+        }
+        else //theres a facedown card in this spot
+        {
+            return drawCard(tempRow*5+tempCol);//continyue turn by drawing that card
+        }
+    }
+
+    public boolean winnerWinner() //checks for win conditions
+    {
+        for (int j = 0; j < cardGrid.length; j++) //array.length returns num rows
+        {       
+            for (int k = 0; k < cardGrid[j].length; k++) 
+            {
+                if (!cardGrid[j][k].getFaceUp() || blankSlot[j][k] ||cardGrid[j][k].getNumValue() != (j*5+k)) //second part as a safeguard
+                    return false;
+            }
+        }
+
+        return true;
     }
 }
