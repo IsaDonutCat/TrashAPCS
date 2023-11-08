ee91dc9d2ff04e44aa394bcb0ad5af1159015e54
IsaDonutCat <114428942+IsaDonutCat@users.noreply.github.com>
2023-11-08 15:55:44 -0500

    Update Trash.java

--- Trash.java
+++ Trash.java
@@ -1,4 +1,28 @@
 public class Trash //so can be combined with other game
 {
-    
+    ArrayList<Card> trashStack;
+    Deck freshStack = new Deck();
+    Grid player1 = new Grid(10, freshStack)
+    Grid player2 = new Grid(10, freshStack);
+
+    public void trashShort ()//create short game
+    {
+        
+    }
+
+    public void trashLong (int play1Num, int play2Num) //create long game
+    {
+        player1 = new Grid(play1Num, freshStack);
+        player2 = new Grid(play2Num, freshStack);
+
+
+    }
+
+    public boolean chickenDinner (int inCount)
+    {
+        if (inCount == 1)
+            return true;
+        else
+            return false;
+    }
 }
